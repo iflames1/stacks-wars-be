b761d2a2b2f9be7274b0c7f1b7e628716fa7f071
Flames <isaacomenuche@gmail.com>
2025-05-26 22:13:03 +0100

    feat: implement turn timer to track turn based and refactor message broadcasting

--- src/ws.rs
+++ src/ws.rs
@@ -9,7 +9,7 @@ use axum::{
 };
 use futures::{SinkExt, StreamExt, stream::SplitSink};
 use std::{collections::HashSet, net::SocketAddr, sync::Arc, time::Duration};
-use tokio::{sync::Mutex, time::timeout};
+use tokio::{sync::Mutex, time::sleep};
 
 use crate::{models::GameRoom, state::AppState};
 use crate::{
@@ -84,15 +84,13 @@ async fn broadcast_to_room(
     if let Some(room) = rooms_guard.get(&room_id) {
         let connection_guard = connections.lock().await;
         for p in &room.players {
-            if p.id != sender_player.id {
-                if let Some(sender_arc) = connection_guard.get(&p.id) {
-                    let mut sender = sender_arc.lock().await;
-                    let _ = sender
-                        .send(Message::Text(
-                            format!("{}: {}", sender_player.id, message).into(),
-                        ))
-                        .await;
-                }
+            if let Some(sender_arc) = connection_guard.get(&p.id) {
+                let mut sender = sender_arc.lock().await;
+                let _ = sender
+                    .send(Message::Text(
+                        format!("{}: {}", sender_player.id, message).into(),
+                    ))
+                    .await;
             }
         }
     }
@@ -107,6 +105,84 @@ async fn store_connection(
     conns.insert(player.id, Arc::new(Mutex::new(sender)));
 }
 
+fn start_turn_timer(
+    player_id: Uuid,
+    room_id: Uuid,
+    rooms: Rooms,
+    connections: Connections,
+    words: Arc<HashSet<String>>,
+) {
+    tokio::spawn(async move {
+        for i in (1..=10).rev() {
+            {
+                let rooms_guard = rooms.lock().await;
+                if let Some(room) = rooms_guard.get(&room_id) {
+                    if room.current_turn_id != player_id {
+                        println!("turn changed, stopping timer");
+                        return;
+                    }
+                } else {
+                    println!("room not found, stopping timer");
+                    return;
+                }
+            }
+
+            let countdown_msg = format!("{} seconds left", i);
+            broadcast_to_room(
+                &Player {
+                    id: player_id,
+                    username: None,
+                },
+                &countdown_msg,
+                room_id,
+                &rooms,
+                &connections,
+            )
+            .await;
+            println!("{} seconds left for player {}", i, player_id);
+            sleep(Duration::from_secs(1)).await;
+        }
+
+        // time ran out
+        let mut rooms_guard = rooms.lock().await;
+        if let Some(room) = rooms_guard.get_mut(&room_id) {
+            if room.current_turn_id == player_id {
+                println!("Player {} timed out", player_id);
+
+                // Find index BEFORE removing the player
+                let current_index = room.players.iter().position(|p| p.id == player_id);
+
+                room.players.retain(|p| p.id != player_id);
+
+                if room.players.is_empty() {
+                    println!("Room {} is now empty", room.id);
+                    return;
+                }
+
+                if let Some(idx) = current_index {
+                    let next_index = if idx >= room.players.len() {
+                        0 // wrap around if needed
+                    } else {
+                        idx
+                    };
+                    let next_id = room.players[next_index].id;
+                    room.current_turn_id = next_id;
+
+                    start_turn_timer(
+                        next_id,
+                        room_id,
+                        rooms.clone(),
+                        connections.clone(),
+                        words.clone(),
+                    );
+                } else {
+                    println!("Couldn't find timed-out player index in room {}", room.id);
+                }
+            }
+        }
+    });
+}
+
 async fn handle_incoming_messages(
     player: &Player,
     room_id: Uuid,
@@ -115,7 +191,7 @@ async fn handle_incoming_messages(
     connections: &Connections,
     words: Arc<HashSet<String>>,
 ) {
-    while let Ok(Some(Ok(msg))) = timeout(Duration::from_secs(15), receiver.next()).await {
+    while let Some(Ok(msg)) = receiver.next().await {
         if let Message::Text(text) = msg {
             println!("Received from {}: {}", player.id, text);
 
@@ -153,6 +229,15 @@ async fn handle_incoming_messages(
                     room.current_turn_id = next_id;
                 }
 
+                // start game loop
+                start_turn_timer(
+                    room.current_turn_id,
+                    room_id,
+                    rooms.clone(),
+                    connections.clone(),
+                    words.clone(),
+                );
+
                 advance_turn = true;
             }
 
@@ -162,30 +247,30 @@ async fn handle_incoming_messages(
         }
     }
 
-    {
-        let mut rooms_guard = rooms.lock().await;
-        if let Some(room) = rooms_guard.get_mut(&room_id) {
-            let was_current_turn = room.current_turn_id == player.id;
+    //{
+    //    let mut rooms_guard = rooms.lock().await;
+    //    if let Some(room) = rooms_guard.get_mut(&room_id) {
+    //        let was_current_turn = room.current_turn_id == player.id;
 
-            println!("player {} timed out", player.id);
+    //        println!("player {} timed out", player.id);
 
-            // Remove the timed-out
-            room.players.retain(|p| p.id != player.id);
+    //        // Remove the timed-out
+    //        room.players.retain(|p| p.id != player.id);
 
-            if room.players.is_empty() {
-                println!("Room {} is empty", room.id);
-                return;
-            }
+    //        if room.players.is_empty() {
+    //            println!("Room {} is empty", room.id);
+    //            return;
+    //        }
 
-            if was_current_turn {
-                if let Some(next_id) = next_turn(&room.players, player.id) {
-                    room.current_turn_id = next_id;
-                } else {
-                    println!("No players left to take the turn in room {}", room.id);
-                }
-            }
-        }
-    }
+    //        if was_current_turn {
+    //            if let Some(next_id) = next_turn(&room.players, player.id) {
+    //                room.current_turn_id = next_id;
+    //            } else {
+    //                println!("No players left to take the turn in room {}", room.id);
+    //            }
+    //        }
+    //    }
+    //}
 }
 
 async fn remove_connection(player: &Player, connections: &Connections) {
