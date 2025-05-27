ec137ae26bcec0650d81016d7123cc1742e2f48c
Flames <isaacomenuche@gmail.com>
2025-05-27 11:32:45 +0100

    refactor: split into main and lib and rename crate name

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,4 @@
-mod models;
-mod state;
-mod ws;
-
-use std::net::SocketAddr;
-
-use state::{AppState, Connections, Rooms};
-
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
-
-    let rooms: Rooms = Default::default();
-    let connections: Connections = Default::default();
-
-    let state = AppState { rooms, connections };
-
-    let app = ws::create_app(state);
-
-    let addr = "127.0.0.1:3001";
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-
-    println!("Websocket server running at ws://{}", addr);
-
-    axum::serve(
-        listener,
-        app.into_make_service_with_connect_info::<SocketAddr>(),
-    )
-    .await
-    .unwrap();
+    stacks_wars_be::start_server().await;
 }
