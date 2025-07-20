0f636db7394c7c91dcb5b388da2027cbfc2965b1
flames <isaacomenuche@gmail.com>
2025-07-20 21:53:20 +0100

    refactor: user functions

---
+++ src/db/user/mod.rs
@@ -0,0 +1,5 @@
+pub mod get;
+pub mod post;
+
+pub use get::get_user_by_id;
+pub use post::create_user;
--- src/db/user/post.rs
+++ src/db/user/post.rs
@@ -65,23 +65,3 @@ pub async fn create_user(
     let token = generate_jwt(&user)?;
     Ok(token)
 }
-
-pub async fn get_user_by_id(id: Uuid, redis: RedisClient) -> Result<User, AppError> {
-    let mut conn = redis.get().await.map_err(|e| match e {
-        bb8::RunError::User(err) => AppError::RedisCommandError(err),
-        bb8::RunError::TimedOut => AppError::RedisPoolError("Redis connection timed out".into()),
-    })?;
-
-    let key = format!("user:{}", id);
-
-    let user_json: String = redis::cmd("GET")
-        .arg(&key)
-        .query_async(&mut *conn)
-        .await
-        .map_err(AppError::RedisCommandError)?;
-
-    let user =
-        serde_json::from_str(&user_json).map_err(|e| AppError::Deserialization(e.to_string()))?;
-
-    Ok(user)
-}
